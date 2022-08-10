734ccc3d098a378102ac6c2219beb5942e2d1869
Sava Pimic <savapimic2@gmail.com>
2022-08-10 03:29:25 +0200

    release

--- components/VideoHero.jsx
+++ components/VideoHero.jsx
@@ -7,7 +7,11 @@ const VideoHero = () => {
     <div className={styles.video_hero}>
       <h1 className={styles.video_heading}>Enjoy your stay</h1>
       <h2 className={styles.secondary_heading}>Come and visit us</h2>
-      <a href="https://www.youtube.com/watch?v=BYLWWeChjJc" target="_blank">
+      <a
+        href="https://www.youtube.com/watch?v=BYLWWeChjJc"
+        target="_blank"
+        rel="noreferrer"
+      >
         <FiPlayCircle className={styles.video_play} />
       </a>
     </div>
