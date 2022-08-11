d007358aadc2871a19c6c1b4f2a803448fb2df0e
Sava Pimic <savapimic2@gmail.com>
2022-08-11 23:06:24 +0200

    Chat Update

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,12 +1,28 @@
 import Layout from "../components/Layout";
 import "../styles/globals.scss";
-import Head from "next/head";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+
+      <Script id="tawk" strategy="lazyOnload">
+        {`
+          var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+          (function(){
+          var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+          s1.async=true;
+          s1.src='https://embed.tawk.to/62f5399737898912e9627a2f/1ga6uhu21';
+          s1.charset='UTF-8';
+          s1.setAttribute('crossorigin','*');
+          s0.parentNode.insertBefore(s1,s0);
+          })();
+        `}
+      </Script>
+    </>
   );
 }
 
